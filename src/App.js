2c2bb59f0d2d7cf29b598d40295897825afcf57e
James Xue <jamesxue100@gmail.com>
2024-12-31 16:57:20 -0800

    Remove basename

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
-// import Contact from "./pages/Contact";
 import './styles.css';
 
 function App() {
   return (
     <div className="app">
-      <Router basename="/newport-sensors">
+      <Router>
         <Header />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/products" element={<Products />} />
-            {/* <Route path="/contact" element={<Contact />} /> */}
           </Routes>
         </main>
         <Footer />
