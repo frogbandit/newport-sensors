5adcfb3a2365fb0f856ce0d3c82b979105e48aa9
James Xue <jamesxue100@gmail.com>
2025-01-03 12:21:26 -0800

    Update styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import homeImage from "../assets/homeImage.jpg";
 
 const Home = () => {
     return (
         <div className="home-container">
-            <div className="home-hero">
+            <div className="overlay"></div> {/* Mask overlay */}
+            <div className="home-content">
                 <h1 className="home-title">
-                    Introducing OSC™ Technology for <span className="highlight">Transportation Safety</span>
+                    Introducing OSC™ Technology for Transportation Safety
                 </h1>
-            </div>
-
-            <div className="home-image">
-                <div className="image-container">
-                    <img src={homeImage} alt="Showcasing transportation" />
-                    <p className="image-credit">
-                        Image by <a href="https://www.istockphoto.com/portfolio/Tryaging?mediatype=photography" target="_blank" rel="noopener noreferrer">Tryaging</a>, from iStockPhoto.com
-                    </p>
-                </div>
-            </div>
-
-            <div className="home-details">
-                <p>
-
-                    Newport Sensors' cutting-edge wireless <strong>Onboard Sensing and Communication (OSC™)</strong> technology platform provides continuous monitoring and real-time alerts, addressing critical transportation safety challenges such as hazardous material tracking, package integrity breaches, and train axle bearing failures.
+                <p className="home-details">
+                    Newport Sensors' cutting-edge wireless <strong>Onboard Sensing and Communication (OSC™)</strong> platform provides continuous monitoring and real-time alerts for problems such as hazardous material tracking, package integrity breaches, and train axle bearing failures.
                 </p>
-            </div>
-
-            <div className="cta-button-container">
-                <Link to="/products" className="cta-button" onClick={() => window.scrollTo(0, 0)}>
-                    Learn More About Our Products
-                </Link>
+                <p className="home-footer">
+                    Newport Sensors, Inc. develops innovative sensor technologies to tackle critical safety challenges.
+                </p>
+                <div className="cta-button-container">
+                    <Link to="/products" className="cta-button" onClick={() => window.scrollTo(0, 0)}>
+                        Learn More
+                    </Link>
+                </div>
             </div>
         </div>
     );
