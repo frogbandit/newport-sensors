cf4b78d792e6ddea68c335bd6b6f2dd3778c1245
James Xue <jamesxue100@gmail.com>
2025-01-22 17:01:26 -0500

    Use hash router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
-import './styles.css';
+import "./styles.css";
 
 function App() {
   return (
